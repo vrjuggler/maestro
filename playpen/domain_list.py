79327cba87206433c04dabb69d05735a64206950
aronb <aronb@d6109375-3c62-66dd-b723-c7798c6675f1>
2006-09-26 19:28:48 +0000

    - Clean up tests a little.

--- playpen/domain_list.py
+++ playpen/domain_list.py
@@ -1,12 +1,15 @@
 import wmi
 
+print "Making a WMI connection..."
 c = wmi.WMI()
+print "Have a WMI connection..."
 
 for comp in c.Win32_ComputerSystem():
    print "ComputerName: ", comp.Name
    print "UserName:", comp.UserName
    print "PartOfDomain: ", comp.PartOfDomain
    print "Domain: ", comp.Domain
+
 
 print "Finding all domains: "
 for i in c.Win32_NTDomain():
--- playpen/process_list.py
+++ playpen/process_list.py
@@ -2,4 +2,10 @@ import wmi
 c = wmi.WMI ()
 for process in c.Win32_Process ():
    (domain, return_value, user) = process.GetOwner()
-   print process.ProcessId, process.Name, user
+   if process.CreationDate is not None:
+      time_tuple = wmi.to_time(process.CreationDate)
+      real_time = "%02d/%02d/%d %d:%02d:%02d" % (time_tuple[1], time_tuple[2], time_tuple[0],
+         time_tuple[3], time_tuple[4], time_tuple[5])
+   else:
+      real_time = ""
+   print process.Name, process.ProcessId, process.ParentProcessId, user, real_time, process.CommandLine
