9a9f36d51d662b82fa3e7621498fee62e4b8d16c
aronb <aronb@d6109375-3c62-66dd-b723-c7798c6675f1>
2006-09-26 15:52:03 +0000

    - Adding a slightly better method.

--- playpen/domain_list.py
+++ playpen/domain_list.py
@@ -1,5 +1,13 @@
 import wmi
 
 c = wmi.WMI()
+
+for comp in c.Win32_ComputerSystem():
+   print "ComputerName: ", comp.Name
+   print "UserName:", comp.UserName
+   print "PartOfDomain: ", comp.PartOfDomain
+   print "Domain: ", comp.Domain
+
+print "Finding all domains: "
 for i in c.Win32_NTDomain():
    print i
