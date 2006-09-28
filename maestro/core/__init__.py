6d20ad5224abb722647f2b2181d8ef9b6ef01999
aronb <aronb@d6109375-3c62-66dd-b723-c7798c6675f1>
2006-09-28 19:53:57 +0000

    - Adding licence headers, no fuctional change.

--- maestro/core/__init__.py
+++ maestro/core/__init__.py
@@ -18,9 +18,7 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 
-
 from environment import Environment
-#from scene_view import SceneViewWidget 
 from plugin_interfaces import IViewPlugin
 #from plugin_holder import ViewPluginsHolder
 
