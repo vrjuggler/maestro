d86e49adc12c918d2ca4b19e0bbf46ceada9ae4a
aronb <aronb@d6109375-3c62-66dd-b723-c7798c6675f1>
2006-09-28 18:30:14 +0000

    - No settings yet.

--- core/__init__.py
+++ core/__init__.py
@@ -1,7 +1,7 @@
 # Package file for core package
 #
 
-from environment import Environment, Settings
+from environment import Environment
 #from scene_view import SceneViewWidget 
 from plugin_interfaces import IViewPlugin
 #from plugin_holder import ViewPluginsHolder
