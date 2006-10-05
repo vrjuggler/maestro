e0bcb8c7722f8f66cc087d401393346ca8ac4fde
patrick <patrick@d6109375-3c62-66dd-b723-c7798c6675f1>
2006-10-05 15:24:46 +0000

    Allow the Maestro GUI to connect to the remote WingIDE debugger at startup. While this hasn't exactly been useful so far (quite the contrary in fact), it might come in handy on a better day.

--- Maestro.py
+++ Maestro.py
@@ -18,6 +18,11 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 
+try:
+   import wingdbstub
+except:
+   pass
+
 import sys, os, os.path, time, traceback
 pj = os.path.join
 from PyQt4 import QtGui, QtCore
