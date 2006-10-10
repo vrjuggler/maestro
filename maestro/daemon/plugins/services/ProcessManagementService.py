cc36d1aff143439279f7e24bbd3985ef5b4fe0af
patrick <patrick@d6109375-3c62-66dd-b723-c7798c6675f1>
2006-10-10 19:39:22 +0000

    Don't print debug output (which can be quite long) when emitting the response to the process.get_procs signal.

--- maestro/daemon/plugins/services/ProcessManagementService.py
+++ maestro/daemon/plugins/services/ProcessManagementService.py
@@ -47,7 +47,7 @@ class ProcessManagementService(maestro.core.IServicePlugin):
       """
       procs = self._getProcs()
       env = maestro.core.Environment()
-      env.mEventManager.emit(nodeId, "process.procs", procs)
+      env.mEventManager.emit(nodeId, "process.procs", procs, debug = False)
 
    def onTerminateProc(self, nodeId, avatar, pid):
       """ Slot that terminates the process that has the given pid.
