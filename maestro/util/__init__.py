109be64b93298d8d8ba2eb19dd5895fb830f84cb
aronb <aronb@d6109375-3c62-66dd-b723-c7798c6675f1>
2006-10-18 06:44:31 +0000

    - Add utility pesudo files that will just send all output to a callback.

--- maestro/util/__init__.py
+++ maestro/util/__init__.py
@@ -17,3 +17,56 @@
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 
 # Module file
+
+class PseudoFile:
+   def __init__(self):
+      """Create a file-like object."""
+      pass
+
+   def readline(self):
+      pass
+
+   def write(self, s):
+      pass
+
+   def writelines(self, l):
+      map(self.write, l)
+
+   def flush(self):
+      pass
+
+   def isatty(self):
+      pass
+
+class PseudoFileIn(PseudoFile):
+   def __init__(self, readline, readlines=None):
+      if callable(readline):
+         self.readline = readline
+      else:
+         raise ValueError, 'readline must be callable'
+      if callable(readlines):
+         self.readlines = readlines
+
+   def isatty(self):
+      return 1
+
+
+class PseudoFileOut(PseudoFile):
+   def __init__(self, write):
+      if callable(write):
+         self.write = write
+      else:
+         raise ValueError, 'write must be callable'
+
+   def isatty(self):
+      return 1
+
+class PseudoFileErr(PseudoFile):
+   def __init__(self, write):
+      if callable(write):
+         self.write = write
+      else:
+         raise ValueError, 'write must be callable'
+
+   def isatty(self):
+      return 1
