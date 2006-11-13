3d69c75306b90c6ea699aef9268fb66c3860a011
patrick <patrick@d6109375-3c62-66dd-b723-c7798c6675f1>
2006-11-13 15:17:29 +0000

    Minor formatting touch ups. No functional changes.

--- maestrod.py
+++ maestrod.py
@@ -13,10 +13,15 @@
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+
+try:
+   import wingdbstub
+except:
+   pass
 
 import sys, os, platform, traceback
 
@@ -174,11 +179,11 @@ if os.name == 'nt':
       def __init__(self, args):
          win32serviceutil.ServiceFramework.__init__(self, args)
          self.mNtEvent = logging.handlers.NTEventLogHandler(self._svc_name_)
-      
+
          const.LOGFILE = os.path.join(os.environ['SystemRoot'], 'system32',
-                                 'maestrod.log')
-         self.mFileLog = logging.handlers.RotatingFileHandler(const.LOGFILE, 'a',
-                                                              50000, 10)
+                                      'maestrod.log')
+         self.mFileLog = logging.handlers.RotatingFileHandler(const.LOGFILE,
+                                                              'a', 50000, 10)
 
       def SvcStop(self):
          sys.stdout = self.savedOut
