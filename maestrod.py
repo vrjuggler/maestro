d71074d97e26a66e2ac310270087b4d0353a0c00
patrick <patrick@d6109375-3c62-66dd-b723-c7798c6675f1>
2006-11-13 15:18:47 +0000

    I did not mean to add the code for loading the Wing IDE debugging stub module, but since it is there now, let's have it be commented out.

--- maestrod.py
+++ maestrod.py
@@ -18,10 +18,10 @@
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
 
-try:
-   import wingdbstub
-except:
-   pass
+#try:
+#   import wingdbstub
+#except:
+#   pass
 
 import sys, os, platform, traceback
 
