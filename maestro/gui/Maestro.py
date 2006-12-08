a8281d4654e96bf44d70e0710ddc2f95c0f0f45f
patrick <patrick@d6109375-3c62-66dd-b723-c7798c6675f1>
2006-12-08 16:41:35 +0000

    Use the Mac OS X platform icon.

--- maestro/gui/Maestro.py
+++ maestro/gui/Maestro.py
@@ -51,6 +51,7 @@ const.mOsIcons[const.ERROR] = QtGui.QIcon(":/Maestro/images/error2.png")
 const.mOsIcons[const.WIN] = QtGui.QIcon(":/Maestro/images/win_xp.png")
 const.mOsIcons[const.WINXP] = QtGui.QIcon(":/Maestro/images/win_xp.png")
 const.mOsIcons[const.LINUX] = QtGui.QIcon(":/Maestro/images/linux2.png")
+const.mOsIcons[const.MACOSX] = QtGui.QIcon(":/Maestro/images/MacOSX.png")
 
 class OutputTabWidget(QtGui.QTabWidget):
    def __init__(self, parent):
