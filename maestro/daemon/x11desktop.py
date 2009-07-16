27a9ad7651143ffdc269f18a518a8edd96ca154a
patrick <patrick@d6109375-3c62-66dd-b723-c7798c6675f1>
2009-07-16 17:14:23 +0000

    Be more careful about execution of xauth(1).

--- maestro/daemon/x11desktop.py
+++ maestro/daemon/x11desktop.py
@@ -75,59 +75,63 @@ def addAuthority(user, xauthCmd, xauthFile):
    # Create the child process.
    pid = os.fork()
    if pid == 0:
-      # Run the xauth(1) command as the authenticated user.
-      maestro.util.changeToUserName(user)
-
-      has_key = 0
-      user_xauth_file = getUserXauthFile(user)
-
       try:
-         (child_stdout, child_stdin) = \
-            popen2.popen2('%s -f %s list' % (xauthCmd, user_xauth_file))
+         # Run the xauth(1) command as the authenticated user.
+         maestro.util.changeToUserName(user)
 
-         # Read the contents of the user's X authority file. This is not done
-         # using readlines() because that could fail due to an interrupted
-         # system call. Instead, we read lines one at a time and handle EINTR
-         # if an when it occurs.
-         lines = maestro.util.readlinesRetryOnEINTR(child_stdout)
-         child_stdout.close()
-         child_stdin.close()
+         has_key = 0
+         user_xauth_file = getUserXauthFile(user)
 
-         # Determine if the user's Xauthority file already has the key.
-         for l in lines:
-            key_match = display_key_re.match(l)
-            if key_match is not None:
-               user_key = (key_match.group(1), key_match.group(2),
-                           key_match.group(3))
-               if user_key == key:
-                  has_key = 1
-                  break
-      # If we fail to determine if the user's .Xauthority file already has the
-      # X11 server key, we will go ahead and attempt to add it.
-      except Exception, ex:
-         print "WARNING: Could not check '%s' for X11 key value: %s" % \
-                  (user_xauth_file, str(ex))
+         try:
+            (child_stdout, child_stdin) = \
+               popen2.popen2('%s -f %s list' % (xauthCmd, user_xauth_file))
 
-      try:
-         # If the user's Xauthority file does not have the key, then we add
-         # it.
-         if not has_key:
-            result = -1
-            count  = 0
-            while result != 0 and count < 10:
-               result = os.spawnl(os.P_WAIT, xauthCmd, xauthCmd, '-f',
-                                  user_xauth_file, 'add', key[0], key[1],
-                                  key[2])
-               count += 1
-      except Exception, ex:
-         print "ERROR: Failed to extend '%s' with X11 server key: %s" % \
-                  (user_xauth_file, str(ex))
+            # Read the contents of the user's X authority file. This is not
+            # done using readlines() because that could fail due to an
+            # interrupted system call. Instead, we read lines one at a time
+            # and handle EINTR if an when it occurs.
+            lines = maestro.util.readlinesRetryOnEINTR(child_stdout)
+            child_stdout.close()
+            child_stdin.close()
+
+            # Determine if the user's Xauthority file already has the key.
+            for l in lines:
+               key_match = display_key_re.match(l)
+               if key_match is not None:
+                  user_key = (key_match.group(1), key_match.group(2),
+                              key_match.group(3))
+                  if user_key == key:
+                     has_key = 1
+                     break
+         # If we fail to determine if the user's .Xauthority file already has
+         # the X11 server key, we will go ahead and attempt to add it.
+         except Exception, ex:
+            print "WARNING: Could not check '%s' for X11 key value: %s" % \
+                     (user_xauth_file, str(ex))
+
+         try:
+            # If the user's Xauthority file does not have the key, then we add
+            # it.
+            if not has_key:
+               result = -1
+               count  = 0
+               while result != 0 and count < 10:
+                  result = os.spawnl(os.P_WAIT, xauthCmd, xauthCmd, '-f',
+                                     user_xauth_file, 'add', key[0], key[1],
+                                     key[2])
+                  count += 1
+         except Exception, ex:
+            print "ERROR: Failed to extend '%s' with X11 server key: %s" % \
+                     (user_xauth_file, str(ex))
+            traceback.print_exc()
+
+         # And that's it for us! It is critical that os._exit() be used here
+         # rather than sys.exit() in order to prevent a SystemExit exception
+         # from being thrown.
+         os._exit(has_key)
+      except:
          traceback.print_exc()
-
-      # And that's it for us! It is critical that os._exit() be used here
-      # rather than sys.exit() in order to prevent a SystemExit exception from
-      # being thrown.
-      os._exit(has_key)
+         os._exit(127)
 
    # Wait on the child to complete. This returns a tuple containing the
    # process ID and the process exit code. The exit code of the child
@@ -136,7 +140,16 @@ def addAuthority(user, xauthCmd, xauthFile):
    child_result = maestro.util.waitpidRetryOnEINTR(pid, 0)
    assert child_result[0] == pid
 
-   return (key[0], bool(child_result[1]))
+   # child_result[1] is a 16-bit value. The high byte is the exit code of the
+   # child process. Valid exit codes are 1 (the user has the X authority key
+   # already) and 0 (the user does not have the X authority key). Anything
+   # else indicates an error.
+   child_exit = child_result[1] >> 8
+
+   if child_exit != 1 and child_exit != 0:
+      raise Exception("X Authority granting process failed: %d" % child_exit)
+
+   return (key[0], bool(child_exit))
 
 def removeAuthority(user, xauthCmd, displayName):
    '''
